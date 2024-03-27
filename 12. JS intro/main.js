4bd9ea4493240db4342bb576ca151ab92d9feb68
elnurxo <elnurykh@code.edu.az>
2024-03-27 16:50:21 +0400

    js intro

---
+++ 12. JS intro/main.js
@@ -0,0 +1,49 @@
+// let fname = "John";
+// let age = 22n;
+// let isMarried = false;
+
+// let x = undefined;
+// let nums = [2,4,5];
+// Array();
+
+// console.log(typeof nums);
+
+//white space " "
+//empty string ""
+
+// let a = 5;
+// let b = 4;
+// let sum = a + b;
+
+// console.log(`sum of ${a} and ${b} is ${sum}`);
+// console.log('sum is ', sum);
+
+// let x = 10;
+// let y = '11';
+
+// console.log(!true)
+
+// x += 3; // x = x + 3;
+// x**=2; // x => 100
+// console.log(x);
+
+//hoisting - variables, function
+
+// var - global scope, function scope, re declare, re assign, can be hoisted
+//let - global scope, block scope, re declare -, re assign, 
+// const - global scope, block scope, re declare -, re assign -
+// display();
+
+console.log(pi);
+const pi = 3.14;
+
+// function display() {
+//     const pi = 2;
+//     console.log(pi);
+// }
+// console.log(pi);
+// display();
+
+
+
+
